08f4a28955a55be05dd88f118e8d9d32f19b0671
DESKTOP-KT196EP\Biljana <biksi1113@gmail.com>
2017-06-13 10:52:19 +0200

    no message

--- src/main/java/korenski/controller/klijenti/KlijentController.java
+++ src/main/java/korenski/controller/klijenti/KlijentController.java
@@ -245,8 +245,8 @@ public class KlijentController {
 		try {
 			
 			Properties connectionProps = new Properties();
-		    connectionProps.put("user", "test");
-		    connectionProps.put("password", "test");
+		    connectionProps.put("user", "root");
+		    connectionProps.put("password", "password");
 		    
 		    Connection conn = DriverManager.getConnection(
 	                   "jdbc:mysql://localhost:3306/finalni?useSSL=false",
